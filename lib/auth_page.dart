d8708500169f438090924c09d2569da035c3552f
Caffeine <45335777+Aya-Ai-2022@users.noreply.github.com>
2023-12-24 18:04:47 +0200

    Update auth_page.dart 1

--- lib/auth_page.dart
+++ lib/auth_page.dart
@@ -1,114 +1,11 @@
 
 
-// Define a widget to display the signup, signin and forgetpassword pages using tabBar
 import 'package:flutter/material.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:final_proj/auth_cubit.dart';
 import 'package:final_proj/forget_password_page.dart';
 import 'package:final_proj/sign_in_page.dart';
 import 'package:final_proj/sign_up_page.dart';
-
-// class AuthPage extends StatefulWidget {
-//   static String id = 'auth-page';
-
-//   const AuthPage({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _AuthPageState createState() => _AuthPageState();
-// }
-
-// class _AuthPageState extends State<AuthPage> {
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Get the user's email from shared preferences and set it to the controller
-//     context.read<AuthCubit>().getEmail().then((value) {
-//       _emailController.text = value;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Flutter App'),
-//         bottom: const TabBar(
-//           tabs: [
-//             Tab(text: 'Sign Up'),
-//             Tab(text: 'Sign In'),
-//             Tab(text: 'Forget Password'),
-//           ],
-//         ),
-//       ),
-//       body: TabBarView(
-//         children: [
-//           SignUpPage(_emailController, _passwordController),
-//           SignInPage(_emailController, _passwordController),
-//           ForgetPasswordPage(_emailController),
-//         ],
-//       ),
-//     );
-//   }
-// }
-// class AuthPage extends StatefulWidget {
-//   static String id = 'auth-page';
-
-//   const AuthPage({super.key});
-
-//   @override
-//   // ignore: library_private_types_in_public_api
-//   _AuthPageState createState() => _AuthPageState();
-// }
-
-// class _AuthPageState extends State<AuthPage> {
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     // Get the user's email from shared preferences and set it to the controller
-//     context.read<AuthCubit>().getEmail().then((value) {
-//       _emailController.text = value;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return DefaultTabController(
-//       length: 3,
-//       child: Scaffold(
-//         appBar: AppBar(
-//           title: const Text('Flutter App'),
-//           bottom: const TabBar(
-//             tabs: [
-//               Tab(text: 'Sign Up'),
-//               Tab(text: 'Sign In'),
-//               Tab(text: 'Forget Password'),
-//             ],
-//           ),
-//         ),
-//         body: TabBarView(
-//           children: [
-//             SignUpPage(_emailController, _passwordController),
-//             SignInPage(_emailController, _passwordController),
-//             ForgetPasswordPage(_emailController),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-
-
-
-
-// SplashScreen with background image
 
 
 // AuthPage with background image
@@ -152,4 +49,3 @@ class AuthPage extends StatelessWidget {
     );
   }
 }
-//C:\Users\aya\StudioProjects\final_proj\assets\images
